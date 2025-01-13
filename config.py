c592662402f4d6e406cad293804f01da67b7907d
blandk77 <alexags1983@gmail.com>
2025-01-13 15:58:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 # Required Variables Config
 API_ID = int(os.environ.get("API_ID", "26728872"))
 API_HASH = os.environ.get("API_HASH", "96985c2aaea6c75408528909b7e18879")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7825342391:AAHcA9HPcf2uEzKWlZwTHKxIksPwIRvPNX4")
 ADMIN = int(os.environ.get("ADMIN", "1705634892"))
 
 
@@ -14,8 +14,8 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", " -1002226481922")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002288135729"))
 
 
 # Mongo DB Database Config
